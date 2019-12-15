3770d7e5ca927010e0c88a18a54354161c314a3c
Miroslav Nikolov <drmnikolov@gmail.com>
2019-12-15 11:50:59 +0200

    Add project files.

---
+++ Aquariums.Tests/AquariumsTests.cs
@@ -0,0 +1,63 @@
+﻿namespace Aquariums.Tests
+{
+    using NUnit.Framework;
+    using System;
+
+    public class AquariumsTests
+    {
+        [Test]
+        public void TestIfCtorWorks()
+        {
+            var aquarium = new Aquarium("Varna", 5);
+
+            Assert.AreEqual("Varna", aquarium.Name);
+            Assert.AreEqual(5, aquarium.Capacity);
+            Assert.AreEqual(0, aquarium.Count);
+        }
+
+        [Test]
+        public void TestIfCtorThrows()
+        {
+            Assert.Throws<ArgumentNullException>(() => new Aquarium(null, 1));
+            Assert.Throws<ArgumentNullException>(() => new Aquarium("", 2));
+            Assert.Throws<ArgumentException>(() => new Aquarium("Varba", -3));
+        }
+
+        [Test]
+        public void TestAdd()
+        {
+            var aquarium = new Aquarium("Varna", 1);
+            aquarium.Add(new Fish("Nemo"));
+            Assert.AreEqual(1, aquarium.Count);
+            Assert.Throws<InvalidOperationException>(() => aquarium.Add(new Fish("Ivanka")));
+        }
+
+        [Test]
+        public void TestRemove()
+        {
+            var aquarium = new Aquarium("Varna", 1);
+            aquarium.Add(new Fish("Nemo"));
+            Assert.Throws<InvalidOperationException>(() => aquarium.RemoveFish("Pesho"));
+            Assert.AreEqual(1, aquarium.Count);
+            aquarium.RemoveFish("Nemo");
+            Assert.AreEqual(0, aquarium.Count);
+        }
+
+        [Test]
+        public void TestSell()
+        {
+            var aquarium = new Aquarium("Varna", 1);
+            aquarium.Add(new Fish("Nemo"));
+            Assert.Throws<InvalidOperationException>(() => aquarium.SellFish("Pesho"));
+            Assert.AreEqual(false, aquarium.SellFish("Nemo").Available);
+        }
+
+        [Test]
+        public void TestReport()
+        {
+            var aquarium = new Aquarium("Varna", 1);
+            aquarium.Add(new Fish("Nemo"));
+            Assert.That(() => aquarium.Report() == "Fish available at Varna: Nemo");
+        }
+    }
+}
---
+++ Aquariums.Tests/FishTests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aquariums.Tests
+{
+    public class FishTests
+    {
+        [Test]
+        public void TestCtor()
+        {
+            var fish = new Fish("Nemo");
+            Assert.AreEqual("Nemo", fish.Name);
+            Assert.AreEqual(true, fish.Available);
+        }
+    }
+}
